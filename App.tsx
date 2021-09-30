a691b01e45724a7894f0f021f3a5fc3baae40dc2
colinclerk <colin@clerk.dev>
2021-09-30 02:41:10 -0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import { ClerkProvider } from "@clerk/clerk-react";
+// import { Clerk } from "@clerk/clerk-js";
+import Clerk from "@clerk/clerk-js/dist/clerk.js";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,7 +18,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <ClerkProvider Clerk={Clerk} frontendApi="clerk.dq2g0.16u6f.lcl.dev">
+          <Navigation colorScheme={colorScheme} />
+        </ClerkProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
